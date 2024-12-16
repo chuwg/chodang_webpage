52b9068cd1d12792bc3a768f13ea0a7b053d266f
chuwg <gi828282@naver.com>
2024-12-17 00:55:58 +0900

    Revert "관리자페이지 오류 수정 및 권한 부여"
    This reverts commit ad275fedbb65f6b0ce4e9fb21e77fa855e89acab.

--- server/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getOrders } = require('../controllers/orderController');
-const { authenticateToken, isAdmin } = require('../middleware/auth');
-
-router.get('/', authenticateToken, isAdmin, getOrders);
-
-module.exports = router; 
